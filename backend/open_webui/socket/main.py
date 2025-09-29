1068f23970d3fe64d989eb0017c63636360fc081
Zac Catalano <zachary.catalano@gmail.com>
2025-09-28 18:24:00 -0700

    update

--- backend/open_webui/socket/main.py
+++ backend/open_webui/socket/main.py
@@ -1,45 +1,38 @@
 import asyncio
-import random
-
-import socketio
 import logging
+import random
 import sys
 import time
 from typing import Dict, Set
-from redis import asyncio as aioredis
-import pycrdt as Y
 
-from open_webui.models.users import Users, UserNameResponse
+import pycrdt as Y
+import socketio
+from open_webui.env import (
+    ENABLE_WEBSOCKET_SUPPORT,
+    GLOBAL_LOG_LEVEL,
+    REDIS_KEY_PREFIX,
+    SRC_LOG_LEVELS,
+    WEBSOCKET_MANAGER,
+    WEBSOCKET_REDIS_CLUSTER,
+    WEBSOCKET_REDIS_LOCK_TIMEOUT,
+    WEBSOCKET_REDIS_URL,
+    WEBSOCKET_SENTINEL_HOSTS,
+    WEBSOCKET_SENTINEL_PORT,
+)
 from open_webui.models.channels import Channels
 from open_webui.models.chats import Chats
 from open_webui.models.notes import Notes, NoteUpdateForm
-from open_webui.utils.redis import (
-    get_sentinels_from_env,
-    get_sentinel_url_from_env,
-)
-
-from open_webui.env import (
-    ENABLE_WEBSOCKET_SUPPORT,
-    WEBSOCKET_MANAGER,
-    WEBSOCKET_REDIS_URL,
-    WEBSOCKET_REDIS_CLUSTER,
-    WEBSOCKET_REDIS_LOCK_TIMEOUT,
-    WEBSOCKET_SENTINEL_PORT,
-    WEBSOCKET_SENTINEL_HOSTS,
-    REDIS_KEY_PREFIX,
-)
-from open_webui.utils.auth import decode_token
+from open_webui.models.users import UserNameResponse, Users
 from open_webui.socket.utils import RedisDict, RedisLock, YdocManager
 from open_webui.tasks import create_task, stop_item_tasks
-from open_webui.utils.redis import get_redis_connection
-from open_webui.utils.access_control import has_access, get_users_with_access
-
-
-from open_webui.env import (
-    GLOBAL_LOG_LEVEL,
-    SRC_LOG_LEVELS,
+from open_webui.utils.access_control import get_users_with_access, has_access
+from open_webui.utils.auth import decode_token
+from open_webui.utils.redis import (
+    get_redis_connection,
+    get_sentinel_url_from_env,
+    get_sentinels_from_env,
 )
-
+from redis import asyncio as aioredis
 
 logging.basicConfig(stream=sys.stdout, level=GLOBAL_LOG_LEVEL)
 log = logging.getLogger(__name__)
@@ -682,7 +675,9 @@ def get_event_emitter(request_info, update_db=True):
                     redis_url=WEBSOCKET_REDIS_URL,
                     lock_name=f"{REDIS_KEY_PREFIX}:message_lock:{request_info['chat_id']}:{request_info['message_id']}",
                     timeout_secs=WEBSOCKET_REDIS_LOCK_TIMEOUT,
-                    redis_sentinels=redis_sentinels,
+                    redis_sentinels=get_sentinels_from_env(
+                        WEBSOCKET_SENTINEL_HOSTS, WEBSOCKET_SENTINEL_PORT
+                    ),
                     redis_cluster=WEBSOCKET_REDIS_CLUSTER,
                 )
 
